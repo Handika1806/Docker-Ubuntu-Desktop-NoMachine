542ed1ecfffb3943a82e773f0d9dd53a7546748c
Hacking Express Grounds <101469168+Handika1806@users.noreply.github.com>
2024-07-27 06:57:47 +0700

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -2,7 +2,6 @@ wget -O ng.sh https://github.com/Handika1806/Docker-Ubuntu-Desktop-NoMachine/raw
 chmod +x ng.sh
 ./ng.sh
 
-
 function goto
 {
     label=$1
@@ -43,4 +42,6 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 604800 | while read i; do echo -en "\r Running .     $i s /604800 s";sleep 0.1;echo -en "\r Running ..    $i s /604800 s";sleep 0.1;echo -en "\r Running ...   $i s /604800 s";sleep 0.1;echo -en "\r Running ....  $i s /604800 s";sleep 0.1;echo -en "\r Running ..... $i s /604800 s";sleep 0.1;echo -en "\r Running     . $i s /604800 s";sleep 0.1;echo -en "\r Running  .... $i s /604800 s";sleep 0.1;echo -en "\r Running   ... $i s /604800 s";sleep 0.1;echo -en "\r Running    .. $i s /604800 s";sleep 0.1;echo -en "\r Running     . $i s /604800 s";sleep 0.1; done
+
+# Keep the script running for 604800 seconds
+sleep 604800
